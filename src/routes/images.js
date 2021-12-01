2b9f47972e076556f0073789b98c3a1a355372d8
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-12-01 19:26:36 -0300

    resolve requisito 10

--- src/routes/images.js
+++ src/routes/images.js
@@ -1,7 +1,10 @@
+const path = require('path');
 const express = require('express');
 const recipesController = require('../controllers/recipes');
 
 const router = express.Router({ mergeParams: true });
+
+router.use(express.static(path.resolve(__dirname, '../uploads')));
 
 router.get('/:id.jpeg', recipesController.findImage);
 
--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,11 +1,8 @@
-const path = require('path');
 const express = require('express');
 const recipesController = require('../controllers/recipes');
 const middleware = require('../middleware');
 
 const router = express.Router({ mergeParams: true });
-
-router.use(express.static(path.resolve(__dirname, '../uploads')));
 
 router.post('/', middleware.auth, recipesController.create);
 router.get('/', recipesController.find);
